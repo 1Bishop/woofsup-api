358e171ce67eb0e1de4d480f8c71fa51641429d1
ninja <iam@chrisbishop.ca>
2019-03-30 02:11:45 -0400

    added gunicorn for heroku

--- app.py
+++ app.py
@@ -45,6 +45,8 @@ def dog_detector(data):
   return ((prediction <= 268) & (prediction >= 151))
 
 
+load_model()
+
 @app.route("/predict", methods=["POST"])
 def predict():
   # initialize the data dictionary that will be returned from the
@@ -84,8 +86,10 @@ def predict():
 
 # if this is the main thread of execution first load the model and
 # then start the server
-if __name__ == "__main__":
-  print(("* Loading Keras model and Flask starting server..."
-    "please wait until server has fully started"))
-  load_model()
-  app.run(debug = False, threaded = False)
+# if __name__ == "__main__":
+#   print(("* Loading Keras model and Flask starting server..."
+#     "please wait until server has fully started"))
+#   load_model()
+#   app.run(debug = False, threaded = False)
+
+app.run(debug = False, threaded = False)
